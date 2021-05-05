bcb678f142960319501c39d934470a8a08bc14fd
wordedburito <72452340+wordedburito@users.noreply.github.com>
2021-05-05 16:40:02 +0100

    Add files via upload

---
+++ assets/js/portfolio.js
@@ -0,0 +1,14 @@
+function monsterclam() {
+    var new_window = window.open("https://www.monsterclam.com/", "_blank");
+    new_window.focus();
+}
+
+function thewordaffairs() {
+    var new_window = window.open("https://www.thewordaffairs.co.uk/", "_blank");
+    new_window.focus();
+}
+
+function wasdllc() {
+    var new_window = window.open("https://www.wasdtech.net/", "_blank");
+    new_window.focus();
+}
